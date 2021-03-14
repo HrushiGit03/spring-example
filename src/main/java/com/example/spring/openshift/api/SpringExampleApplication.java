ca0da0d474513fcef189ba7ef0616b183d224a5c
HrushiGit03 <50804942+HrushiGit03@users.noreply.github.com>
2021-03-14 15:08:43 +0530

    Add files via upload
    Openshift deployment testing

---
+++ src/main/java/com/example/spring/openshift/api/SpringExampleApplication.java
@@ -0,0 +1,32 @@
+package com.example.spring.openshift.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringExampleApplication {
+	
+	@GetMapping("/")
+	public String welcome()
+	{
+		
+		return "Welcome to Our Openshift deployment";
+	}
+	
+	@GetMapping("/input")
+	public String congrats(@PathVariable String input)
+	{
+		
+		return "Hi"+input+"Your application deployed successfully in Openshift container";
+	}
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+
+}
